eec66b48714271d4ca18d13a021d801e1b9d0e6d
Karen Gutierrez <karengutierrez@MacBook-Pro.(none)>
2021-09-04 13:28:11 -0600

    adding multi-marker map

---
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,37 @@
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([37.0902, -95.7129],4);
+
+// Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop thru the cities array and create a marker for each
+
+cityData.forEach(function(city){
+    console.log(city);
+    L.circleMarker(city.location, {
+        radius: city.population/100000,
+        color: 'orange',
+        weight: 4
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
+
+// Add a circle to the map
+// L.circleMarker([34.0522, -118.2437], {
+//     radius: 300,
+//     color: 'black',
+//     fillColor: '#ffffa1',
+// }).addTo(map);
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
